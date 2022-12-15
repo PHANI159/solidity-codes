1cecf25e1510fc39b06e1166c444ffbff46328d3
Phani Chandra <115294163+phanii159@users.noreply.github.com>
2022-12-15 11:33:13 +0530

    Add files via upload
    Hello world program in solidity

---
+++ 01.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.17;
+contract abc {
+    string public a="Hello World one!";
+    string public b="Hello World two!";
+    string public c="Hello World three!";
+    string public d="Hello World four!";
+    string public e="Hello World five!";
+}
